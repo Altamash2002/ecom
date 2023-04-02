1f812fa7c3d62e7085be747a019b6a4df475e418
Altamash <altamashshaikh_ce_2020@ltce.in>
2023-04-02 16:11:06 +0530

    feat : added Slider in the home page

--- client/src/components/mainpages/home/Home.js
+++ client/src/components/mainpages/home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './home.css' 
+import Slider from './Slider'
 
 const Home = () => {
   return (
     <div>
-
+      <Slider/>
     </div>
   )
 }
---
+++ client/src/components/mainpages/home/Slider.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import "./slider.css"
+
+const images = [
+  {
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg',
+    alt: 'image 1',
+    caption: 'Caption Text 1'
+  },
+  {
+    src: 'https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg',
+    alt: 'image 2',
+    caption: 'Caption Text 2'
+  },
+  {
+    src: 'https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg',
+    alt: 'image 3',
+    caption: 'Caption Text 3'
+  },
+  {
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg',
+    alt: 'image 4',
+    caption: 'Caption Text 4'
+  }
+];
+
+const Slider = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleArrowClick = (index) => {
+    setActiveIndex((prevIndex) => {
+      let newIndex = prevIndex + index;
+      if (newIndex < 0) {
+        newIndex = images.length - 1;
+      } else if (newIndex >= images.length) {
+        newIndex = 0;
+      }
+      return newIndex;
+    });
+  };
+
+  return (
+    <div id="container">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`slides ${index === activeIndex ? 'active' : ''}`}
+        >
+          <img src={image.src} alt={image.alt} />
+          <div className="caption">{image.caption}</div>
+        </div>
+      ))}
+      <span
+        className="arrow"
+        id="left"
+        onClick={() => handleArrowClick(-1)}
+      >
+        &lt;
+      </span>
+      <span
+        className="arrow"
+        id="right"
+        onClick={() => handleArrowClick(1)}
+      >
+        &#62;
+      </span>
+    </div>
+  );
+};
+
+export default Slider;
